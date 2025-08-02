f8e75cc35b4132a2f5bc04f0b7a29cc437104686
psndna88 <psndna88@gmail.com>
2025-08-02 22:39:35 +0530

    susfs upgrade to v1.5.10
    https://github.com/JackA1ltman/NonGKI_Kernel_Build/tree/main/Patches/Patch

--- fs/exec.c
+++ fs/exec.c
@@ -66,6 +66,9 @@
 #include <linux/sched.h>
 
 #include <linux/uaccess.h>
+#ifdef CONFIG_KSU_SUSFS_SUS_SU
+#include <linux/susfs_def.h>
+#endif
 #include <asm/mmu_context.h>
 #include <asm/tlb.h>
 
@@ -1945,6 +1948,7 @@ extern int ksu_handle_execveat_sucompat(int *fd, struct filename **filename_ptr,
 
 #ifdef CONFIG_KSU_SUSFS_SUS_SU
 extern bool susfs_is_sus_su_hooks_enabled __read_mostly;
+extern bool __ksu_is_allow_uid(uid_t uid);
 extern int ksu_handle_execveat_sucompat(int *fd, struct filename **filename_ptr, void *argv,
 				void *envp, int *flags);
 #endif
@@ -1961,8 +1965,15 @@ static int do_execveat_common(int fd, struct filename *filename,
 		ksu_handle_execveat_sucompat(&fd, &filename, &argv, &envp, &flags);
 #endif
 #ifdef CONFIG_KSU_SUSFS_SUS_SU
-	if (susfs_is_sus_su_hooks_enabled)
+	if (likely(susfs_is_current_proc_root_not_allowed())) {
+		goto orig_flow;
+	}
+	if (likely(susfs_is_sus_su_hooks_enabled) &&
+		unlikely(__ksu_is_allow_uid(current_uid().val)))
+	{
 		ksu_handle_execveat_sucompat(&fd, &filename, &argv, &envp, &flags);
+	}
+orig_flow:
 #endif
 	return __do_execve_file(fd, filename, argv, envp, flags, NULL);
 }

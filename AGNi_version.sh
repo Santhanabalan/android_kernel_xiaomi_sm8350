3c9d369cb163adbdaa8cfde40b95973eba0136e5
psndna88 <psndna88@gmail.com>
2025-10-05 17:05:37 +0530

    AGNi AOSP v7.13 stable [5.4.300]

--- AGNi_version.sh
+++ AGNi_version.sh
@@ -3,9 +3,9 @@
 ## AGNi version info
 KERNELDIR=`readlink -f .`
 
-export AGNI_VERSION="v7.12"
-sed -i 's/5.4.298/5.4.299/' $KERNELDIR/arch/arm64/configs/agni_*
-sed -i 's/v7.11-stable/v7.12-stable/' $KERNELDIR/arch/arm64/configs/agni_*
+export AGNI_VERSION="v7.13"
+sed -i 's/5.4.299/5.4.300/' $KERNELDIR/arch/arm64/configs/agni_*
+sed -i 's/v7.12-stable/v7.13-stable/' $KERNELDIR/arch/arm64/configs/agni_*
 
 echo "	AGNi Version info loaded."
 
